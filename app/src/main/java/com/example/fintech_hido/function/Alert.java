56fd0ca43e71a62875e751c9be5c26516f3b4fa8
dlthgml1997 <shfksthgml@naver.com>
2020-08-24 14:48:45 +0900

    refactor: Change if to switch

--- app/src/main/java/com/example/fintech_hido/function/Alert.java
+++ app/src/main/java/com/example/fintech_hido/function/Alert.java
@@ -12,77 +12,81 @@ public class Alert extends AppCompatActivity {
     public static void alert_function(Context context, String mode) {
         builder = new AlertDialog.Builder(context);
 
-        if(mode.equals("loading")){
-            builder.setTitle("알림").setMessage("서버의 응답이 없습니다");
-            setneutralButton("exit",context);
-        }
-        else if(mode.equals("fail")){
-            builder.setTitle("알림").setMessage("요청이 실패했습니다");
-            setneutralButton("normal",context);
-        }
-        else if(mode.equals("register")){
-            builder.setTitle("알림").setMessage("지문정보가 이미 존재합니다");
-            setneutralButton("register",context);
-        }
-        else if(mode.equals("register_result")){
-            builder.setTitle("알림").setMessage("지문정보 등록에 실패했습니다");
-            setneutralButton("register_result",context);
-        }
-        else if(mode.equals("auth")){
-            builder.setTitle("알림").setMessage("지문정보가 없습니다");
-            setneutralButton("auth_function",context);
-        }
-        else if(mode.equals("auth_result")){
-            builder.setTitle("알림").setMessage("인증에 실패했습니다");
-            setneutralButton("result",context);
+        switch (mode) {
+            case "loading":
+                builder.setTitle("알림").setMessage("서버의 응답이 없습니다");
+                setneutralButton("exit", context);
+                break;
+            case "fail":
+                builder.setTitle("알림").setMessage("요청이 실패했습니다");
+                setneutralButton("normal", context);
+                break;
+            case "register":
+                builder.setTitle("알림").setMessage("지문정보가 이미 존재합니다");
+                setneutralButton("register", context);
+                break;
+            case "register_result":
+                builder.setTitle("알림").setMessage("지문정보 등록에 실패했습니다");
+                setneutralButton("register_result", context);
+                break;
+            case "auth":
+                builder.setTitle("알림").setMessage("지문정보가 없습니다");
+                setneutralButton("auth_function", context);
+                break;
+            case "auth_result":
+                builder.setTitle("알림").setMessage("인증에 실패했습니다");
+                setneutralButton("result", context);
+                break;
         }
     }
 
     private static void setneutralButton(String mode, Context para_context)
     {
         final Context context = para_context;
-        if(mode.equals("exit")){
-            builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    System.runFinalization();
-                    System.exit(0);
-                }
-            });
-        }
-        else if(mode.equals("normal")){
-            builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                }
-            });
-        }
-        else if(mode.equals("register")){
-            builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Fingerprint_function fingerprint_function = (Fingerprint_function)context;
-                    fingerprint_function.call_back(false);
-                }
-            });
-        }
-        else if(mode.equals("result")){
-            builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Fingerprint_function fingerprint_function = (Fingerprint_function)context;
-                    fingerprint_function.return_result(false);
-                }
-            });
-        }
-        else if(mode.equals("auth_function")){
-            builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Fingerprint_function fingerprint_function = (Fingerprint_function)context;
-                    fingerprint_function.auth_function(false);
-                }
-            });
+        switch (mode) {
+            case "exit":
+                builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.runFinalization();
+                        System.exit(0);
+                    }
+                });
+                break;
+            case "normal":
+                builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                break;
+            case "register":
+                builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Fingerprint_function fingerprint_function = (Fingerprint_function) context;
+                        fingerprint_function.call_back(false);
+                    }
+                });
+                break;
+            case "result":
+                builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Fingerprint_function fingerprint_function = (Fingerprint_function) context;
+                        fingerprint_function.return_result(false);
+                    }
+                });
+                break;
+            case "auth_function":
+                builder.setNeutralButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Fingerprint_function fingerprint_function = (Fingerprint_function) context;
+                        fingerprint_function.auth_function(false);
+                    }
+                });
+                break;
         }
         alert_show();
     }
